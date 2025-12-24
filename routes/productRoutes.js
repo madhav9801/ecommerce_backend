babcf54dbcade8583b1c3684d0de3ddd717b3533
Madhav Agarwal <madhavagarwal067@gmail.com>
2025-12-24 21:29:47 +0530

    changes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
 const { getProducts, addProduct } = require("../controllers/productController");
 const authMiddleware = require("../middleware/authMiddleware");
-const adminMiddleware = require("../middleware/adminMiddleware");
+// const adminMiddleware = require("../middleware/adminMiddleware");
 
 router.get("/", getProducts);      // Public
 router.post(
   "/",
   authMiddleware,
-  adminMiddleware,
   addProduct
 );     
 module.exports = router;
